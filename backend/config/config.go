b00241d2d3b40dba3b383e2b780032bea10ea525
ericluec <eric.luecking@gmail.com>
2025-04-02 17:58:29 +0200

    simple func added

---
+++ backend/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectToMongoDB() (*mongo.Client, error) {
+	uri := "mongodb://localhost:27017"
+	if uri == "" {
+		return nil, fmt.Errorf("MONGODB_URI is not set")
+	}
+
+	clientOptions := options.Client().ApplyURI(uri)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
--- backend/services/tableService.go
+++ backend/services/tableService.go
@@ -1,1 +1,63 @@
 package services
+
+import (
+	"bbb-lars/config"
+	"bbb-lars/entities"
+	"context"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
+)
+
+func CreateTable(w http.ResponseWriter, r *http.Request) {
+	client, err := config.ConnectToMongoDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	var table entities.Table
+	if err := json.NewDecoder(r.Body).Decode(&table); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	collection := client.Database("bbb-lars").Collection("table")
+	result, err := collection.InsertOne(context.Background(), table)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
+
+func GetAllTables(w http.ResponseWriter, r *http.Request) {
+	client, err := config.ConnectToMongoDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer client.Disconnect(context.Background())
+
+	collection := client.Database("bbb-lars").Collection("table")
+	cursor, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(context.Background())
+
+	var tables []entities.Table
+	for cursor.Next(context.Background()) {
+		var table entities.Table
+		if err := cursor.Decode(&table); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tables = append(tables, table)
+	}
+
+	json.NewEncoder(w).Encode(tables)
+}
