7a2bb3bf7bc90e5b785d8e398e0f9a65aa88bb4d
ericluec <eric.luecking@gmail.com>
2025-04-02 16:49:53 +0200

    added new struc

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"bbb-lars/backend/handlers"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/table", handlers.CreateTable)
 
 	fmt.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
